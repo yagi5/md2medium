f1b04ca570fb3816c65f8660d01651a076bf046e
ygnmhdtt <ygnmhdtt@gmail.com>
2018-07-07 15:15:30 +0900

    add cli module

--- main.go
+++ main.go
@@ -4,9 +4,48 @@ import (
 	"os"
 
 	medium "github.com/medium/medium-sdk-go"
+	"github.com/urfave/cli"
 )
 
 func main() {
+	app := cli.NewApp()
+	app.Name = "md2medium"
+	app.Usage = "publish your markdown to medium"
+	app.Version = "1.0.1"
+	app.Flags = flags()
+	app.Action = run
+	app.Run(os.Args)
+}
+
+func flags() []cli.Flag {
+	flags := []cli.Flag{
+		cli.BoolFlag{
+			Name:  "draft",
+			Usage: "if this flag is true, files will be published as draft.",
+		},
+		cli.StringFlag{
+			Name:  "title",
+			Usage: "specify title",
+		},
+		cli.StringFlag{
+			Name:  "file",
+			Usage: "specify file path",
+		},
+	}
+	return flags
+}
+
+func run(c *cli.Context) error {
+	title := c.String("title")
+	file := c.String("file")
+	isDraft := c.Bool("draft")
+	fmt.Println(title)
+	fmt.Println(file)
+	fmt.Println(isDraft)
+	return publish(title, file, isDraft)
+}
+
+func publish(title string, file string, isDraft bool) error {
 	m := medium.NewClientWithAccessToken(os.Getenv("MEDIUM_ACCESS_TOKEN"))
 
 	// empty means current user
